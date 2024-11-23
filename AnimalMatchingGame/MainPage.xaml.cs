aff15ddd2fff8a6b8ac8252f07168e0e0020e645
mhmtfth13 <mhmtfthaydin@gmail.com>
2024-11-23 16:46:47 +0300

    Add project files.

---
+++ AnimalMatchingGame/MainPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace AnimalMatchingGame
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void PlayAgainButton_Clicked(object sender, EventArgs e)
+        {
+            AnimalsButtons.IsVisible= true;
+            PlayAgainButton.IsVisible= false;
+
+            List<string> animalEmoji = [
+                "🐼", "🐼",
+                "🐦‍⬛","🐦‍⬛",
+                "🐿️","🐿️",
+                "🦄","🦄",
+                "🐈‍⬛","🐈‍⬛",
+                "🐮","🐮",
+                "🐮","🐮",
+                "🦈","🦈"
+            ];
+            foreach (var button in AnimalsButtons.Children.OfType<Button>())
+            { 
+                int index = Random.Shared.Next(animalEmoji.Count);
+                string nextEmoji = animalEmoji[index];
+                button.Text = nextEmoji;
+                animalEmoji.RemoveAt(index);
+            }
+
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+}
