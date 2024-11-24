0b99ed1198031be4be27d416feed451fccfd62b7
mhmtfth13 <mhmtfthaydin@gmail.com>
2024-11-24 14:55:38 +0300

    Fixing reseting backgroundcolor bug. (When the restart game. Buttons not reset background color. )

--- AnimalMatchingGame/MainPage.xaml.cs
+++ AnimalMatchingGame/MainPage.xaml.cs
@@ -5,10 +5,15 @@ namespace AnimalMatchingGame
     public partial class MainPage : ContentPage
     {
         int count = 0;
+        Button lastClicked;
+        bool findingMatch = false;
+        int matchesFound;
 
         public MainPage()
         {
             InitializeComponent();
+            //PlayAgainButton_Clicked(null, null);
+            //PlayAgainButton.IsVisible = false;
         }
 
         private void PlayAgainButton_Clicked(object sender, EventArgs e)
@@ -23,7 +28,7 @@ namespace AnimalMatchingGame
                 "🦄","🦄",
                 "🐈‍⬛","🐈‍⬛",
                 "🐮","🐮",
-                "🐮","🐮",
+                "🦩","🦩",
                 "🦈","🦈"
             ];
             foreach (var button in AnimalsButtons.Children.OfType<Button>())
@@ -38,7 +43,37 @@ namespace AnimalMatchingGame
 
         private void Button_Clicked(object sender, EventArgs e)
         {
-
+            if (sender is Button buttonClicked)
+            {
+                if (!string.IsNullOrEmpty(buttonClicked.Text) && (findingMatch == false))
+                {
+                    buttonClicked.Background = Colors.Red;
+                    lastClicked = buttonClicked;
+                    findingMatch = true;
+                }
+                else
+                {
+                    if ((buttonClicked != lastClicked) && (buttonClicked.Text == lastClicked.Text) && (!String.IsNullOrWhiteSpace(buttonClicked.Text)))
+                    {
+                        matchesFound++;
+                        lastClicked.Text = " ";
+                        buttonClicked.Text = " ";
+                    }
+                    lastClicked.Background = Colors.LightGreen;
+                    buttonClicked.BackgroundColor = Colors.LightGreen;
+                    findingMatch = false;
+                }
+            }
+            if (matchesFound == 8)
+            { 
+                matchesFound = 0;
+                AnimalsButtons.IsVisible = false;
+                PlayAgainButton.IsVisible = true;
+                foreach (var button in AnimalsButtons.Children.OfType<Button>())
+                {
+                    button.Background = Colors.LightBlue;
+                }
+            }
         }
     }
 
